aa236edc25c2459d645109017ed419b71535ee9c
Калинин Павел <pasha.kalinin.05@mail.ru>
2023-09-11 21:36:02 +0300

    новое занятие

---
+++ lesson-number-2/task3.py
@@ -0,0 +1,9 @@
+import cmath
+x = -2.235 * 10**-2
+y = 2.23
+z = 15.221
+a = (cmath.exp(abs(x-y))) * ((abs(x-y))**(x+y))
+b = cmath.atan(x) + cmath.atan(z)
+c = (x**6 + cmath.log10(y)**2)**(1/3)
+s = a/b + c
+print(s)
