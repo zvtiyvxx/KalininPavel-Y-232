2e74e6aed9bef5e98b9dc05cee667c4ac598b456
zvtiyvxx <pasha.kalinin.05@mail.ru>
2023-10-03 18:18:16 +0300

    Update Задача 1.py

--- Занятие 4/Задача 1.py
+++ Занятие 4/Задача 1.py
@@ -1,8 +1,10 @@
 # -- coding: utf-8 --
+def gg(a,b):
+    while a < b:
+    a += 1
 a = int(input('Введите число a '))
 b = int(input('Введите число b (должно быть больше числа a или равно ему) '))
-print(a)
-while a < b:
-    a += 1
-    print(a)
+c = gg(a,b)
+print(c)
+
 
