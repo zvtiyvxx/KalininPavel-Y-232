763f43a9eb607a7cf9f5b463f67016b2f6fed09c
zvtiyvxx <pasha.kalinin.05@mail.ru>
2023-10-03 17:46:41 +0300

    Update Задача 7.py

--- Занятие 3/Задача 7.py
+++ Занятие 3/Задача 7.py
@@ -1,9 +1,9 @@
 # -- coding: utf-8 --
 def visocosniu(x):
     if ((x % 4 == 0) and (x % 100 != 0)) or (x % 400 == 0):
-        return(print('Да'))
+        return('Да')
     else:
-        return(print('Нет'))
+        return('Нет')
 x = int(input('Введите год: '))
 c = visocosniu(x)
 print(c)
