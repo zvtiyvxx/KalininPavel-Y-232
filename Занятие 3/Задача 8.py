e4c9f585ca65750d0ce5d7440f38c24a05c14f01
zvtiyvxx <pasha.kalinin.05@mail.ru>
2023-10-03 17:43:30 +0300

    Update Задача 8.py

--- Занятие 3/Задача 8.py
+++ Занятие 3/Задача 8.py
@@ -1,14 +1,13 @@
 # -- coding: utf-8 --
+def sovpad(a,b,c):
+    if a == b == c:
+        return(3)
+    elif a == b or b == c or a == c:
+        return(2)
+    else:
+        return(3)
 a = int(input('Введите первое число: '))
 b = int(input('Введите второе число: '))
 c = int(input('Введите третье число: '))
-def sovpad(a,b,c):
-    if a == b == c:
-        print('3')
-    elif a == b or b == c or a == c:
-        print('2')
-    else:
-        print('3')
-        return
 c = sovpad(a,b,c)
 print(c)
