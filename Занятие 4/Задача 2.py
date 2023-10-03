7a76280b31732de487a21992564b2b199ac668e7
zvtiyvxx <pasha.kalinin.05@mail.ru>
2023-10-03 18:19:37 +0300

    Update Задача 2.py

--- Занятие 4/Задача 2.py
+++ Занятие 4/Задача 2.py
@@ -1,10 +1,12 @@
 # -- coding: utf-8 --
-a = int(input('Введите число a '))
-b = int(input('Введите число b '))
-while a != b:
+def gg(a,b):
+    while a != b:
     print(a)
     if a < b:
         a += 1
     elif b < a:
         a -= 1
+a = int(input('Введите число a '))
+b = int(input('Введите число b '))
+c = gg(a,b)
 print(b)
