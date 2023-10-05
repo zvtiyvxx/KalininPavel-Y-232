5894026bf93e95ad96d5e74cc06f8029f6e31a28
zvtiyvxx <pasha.kalinin.05@mail.ru>
2023-10-05 16:13:01 +0300

    Update Задача 4.py

--- Занятие 4/Задача 4.py
+++ Занятие 4/Задача 4.py
@@ -1,9 +1,10 @@
 # -- coding: utf-8 --
+def gg(n):
+    d = 0
+    for i in range(n):
+        b = int(input('Введите каждое из чисел: '))
+        d += b
+    return d
 n = int(input("Введите количество чисел: "))
-c = 0
-
-for i in range(n):
-    b = int(input('Введите каждое из чисел: '))
-    c += b
-
+c = gg(n)
 print(c)
