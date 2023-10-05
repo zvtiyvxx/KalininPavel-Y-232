dd525b2082d7fb6c8a2cee2cf65f472e7d99cabf
zvtiyvxx <pasha.kalinin.05@mail.ru>
2023-10-05 16:37:03 +0300

    Update Задача 9.py

--- Занятие 4/Задача 9.py
+++ Занятие 4/Задача 9.py
@@ -1,11 +1,12 @@
 # -- coding: utf-8 --
+def gg(n):
+    a = 0
+    b = 1
+    s = 0
+    for i in range(n):
+        s += a
+        a, b = b, a + b 
+    return s
 n = int(input("Введите количество чисел Фибоначчи: "))
-a = 0
-b = 1
-s = 0
-
-for i in range(n):
-    s += a
-    b = b
-    a + b
-print(i)
+c = gg(n)
+print(c)
