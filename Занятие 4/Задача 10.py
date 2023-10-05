c92e012994ae3ad842282a1f99e244cb0918e764
zvtiyvxx <pasha.kalinin.05@mail.ru>
2023-10-05 16:06:44 +0300

    Update Задача 10.py

--- Занятие 4/Задача 10.py
+++ Занятие 4/Задача 10.py
@@ -1,7 +1,10 @@
 # -- coding: utf-8 --
+def gg(n,k):
+    fib = [0, 1]
+    for i in range(2, n + 1):
+        fib.append(fib[i - 1] + fib[i - 2])
+        return(sum(fib[k-1:]))
 n = int(input('Введите число n: '))
 k = int(input('Введите число k: '))
-fib = [0, 1]
-for i in range(2, n + 1):
-    fib.append(fib[i - 1] + fib[i - 2])
-print(sum(fib[k-1:]))
+c = gg(n,k)
+print(c)
