3626d9ee4f8147cfe1ad185cc13255d6348d88b9
zvtiyvxx <pasha.kalinin.05@mail.ru>
2023-10-05 16:28:18 +0300

    Update Задача 8.py

--- Занятие 4/Задача 8.py
+++ Занятие 4/Задача 8.py
@@ -1,6 +1,8 @@
 # -- coding: utf-8 --
+def gg(n):
+    for i in range(n,0,-1):
+        for j in range(1, i + 1):
+            print(j, end="")
+        print()
 n = int(input("Введите число n (n <= 9): "))
-for i in range(1, n + 1):
-    for j in range(1, i + 1):
-        print(j, end="")
-    print()
+gg(n)
